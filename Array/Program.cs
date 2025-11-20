7e7ce7566880f8288047ac0abd08252c083de3b4
DESKTOP-155CKK6\EZEN <ashleyriot000@gmail.com>
2025-11-20 16:27:40 +0900

    배열이란
    - 배열의 소개, 선언 및 정의
    - 요소 접근 및 대입 방법

---
+++ Array/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열 선언, 정의 방식. 생략이 가능함.
+            int[] iArray;
+            int[] iArray2 = new int[100];
+            int[] iArray3 = new int[5]
+            {
+                0,1,2,3,4
+            };
+
+
+            //배열 요소에 접근, 대입하는 방법
+            int[] iArray4 = {2,3,4,56};
+            iArray4[0] = 5;
+            iArray4[3] = iArray4[0];
+            int temp = iArray4[0];
+            iArray4[0] = iArray4[3];
+            iArray4[3] = temp;
+
+            //2차원 배열 선언 및 정의
+            long[,] lArray = new long[5, 5]
+            {
+                { 0, 1, 2, 3, 4},
+                { 0, 1, 2, 3, 4},
+                { 0, 1, 2, 3, 4},
+                { 0, 1, 2, 3, 4},
+                { 0, 1, 2, 3, 4}
+            };
+
+            //2차원 배열의 대입
+            lArray[0, 1] = 10;
+
+
+            //가변배열, 지그배열의 선언, 정의 방법(잘 안쓰이는 배열)
+            short[][] sArray = new short[3][];
+            sArray[0] = new short[5];
+            sArray[1] = new short[10];
+            sArray[2] = new short[3];
+
+            int[] sumArray = new int[10]
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+            int result = sumArray[0] + sumArray[1] + sumArray[2] + sumArray[3] + sumArray[4] +
+                sumArray[5] + sumArray[6] + sumArray[7] + sumArray[8] + sumArray[9];
+
+            Console.WriteLine(result);
+        }
+    }
+}
--- Variables/Program.cs
+++ Variables/Program.cs
@@ -14,11 +14,17 @@
 
             int iC = Sum(iA, iB);
 
+            //명시적 형변환
             short sA = (short)iA;
+            short sB = 15;
+            short sC = (short)(sA + sB);
+
+            //암묵적 형변환
             long lA = iA;
             float fA = lA;
 
-            float fB = 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f;
+            //실수형의 정밀도 문제 주의
+            float fB = 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f;            
             if(fB == 1f)
             {
                 Console.WriteLine("fB는 1과 같습니다.");
@@ -27,14 +33,26 @@
             {
                 Console.WriteLine("fB는 1과 다릅니다.");
             }
+
+            //형이 다른 숫자 변수들끼리 연산할 경우 범위가 큰 타입으로 암묵적 형변환이 일어나고
+            //결과값도 범위가 큰 타입으로 반환된다.
+           float fC = lA + fA;
+
+            //참, 거짓만 판별
+            bool isTrue = true;
+            isTrue = false;
+
+            //문자 하나만 담을 수 있는 변수
+            char a = 'a';
+            char b = (char)(a + 1);
+            //원하는 길이의 문장을 담을 수 있는 변수
+            string text = "안녕하세요";
         }
 
+        //매개변수l과 r의 더한 값을 반환하는 함수 
         static int Sum(int l, int r)
         {
             return l + r;
         }
     }
-
-
-    
 }
