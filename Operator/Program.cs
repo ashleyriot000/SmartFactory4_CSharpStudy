fd738d7b660084953fd8e9c8dea39f903e8089c8
DESKTOP-155CKK6\EZEN <ashleyriot000@gmail.com>
2025-11-20 19:14:59 +0900

    연산자란
    - 연산자 설명
    - if문 설명 및 예제 실습

---
+++ Operator/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //+,-,*,/,%
+            int iA = 5;
+            int iB = 16;
+
+            int iPlus = iA + iB;
+            int iMinus = iA - iB;
+            int iMultifly = iA * iB;
+            int iDivide = iA / iB;
+            int iRemain = iA % iB;
+
+            //증감 연산자
+            int iC = iPlus++;
+            int iD = ++iPlus;
+            
+            //iC = iPlus;
+            //iPlus = iPlus + 1;
+            iPlus += 1;
+
+            --iPlus;
+            iPlus--;
+
+            //비교연산자
+            bool bSame = iPlus == iMinus;
+            bool bNotSame = iPlus != iMinus;
+            bool bBigger = iPlus > iMinus;
+            bool bBiggerOrSame = iPlus >= iMinus;
+            bool bSmaller = iPlus < iMinus;
+            bool bSmallerOrSame = iPlus <= iMinus;
+
+            //논리 연산자
+            //부정 연산자
+            bool isTrue = false;
+            isTrue = !bSame;
+            isTrue = !isTrue;
+
+            //And 연산자
+            bool and = bSame & bBigger & bSmallerOrSame;
+            and = bSame && bBigger && bSmallerOrSame;
+
+
+            //Or 연산자
+            bool or = bSame | bBigger | bSmaller;
+            or = bSame || bBigger || bSmaller;
+
+            //XOR 연산자
+            bool xor = bSame ^ bBigger;
+
+            //삼항 연산자
+            int iResult = bSame & bSmaller ? 1 : 0;
+
+
+            //if문
+            if(bSame)
+            {
+                //참이면 호출
+                Console.WriteLine("bSame은 정말로 참입니다.");
+            }
+            else
+            {
+                //거짓이면 호출
+                Console.WriteLine("bSame은 거짓입니다.");
+            }
+
+            if(bBigger)
+            {
+                //참이면 호출
+                Console.WriteLine("bBigger는 참입니다.");
+            }
+            else if(bSame)
+            {
+                //참이면 호출
+                Console.WriteLine("bSame은 참입니다.");
+
+            }
+            else
+            {
+                //모두 아니면 호출
+                Console.WriteLine("bBigger도 bSame도 거짓입니다.");
+            }
+
+
+            Console.WriteLine("======================================");
+            Console.Write("정수 하나를 입력해주세요 : ");
+            string text = Console.ReadLine();
+            if(int.TryParse(text, out int result))
+            {
+                if(result % 2 == 0)
+                {
+                    Console.WriteLine($"그래 {result}는 짝수가 맞아");
+                    //Console.WriteLine("그래 " + result + "는 짝수가 맞아");                    
+                }
+                else
+                {
+                    Console.WriteLine($"아니야, {result}는 짝수가 아니야");
+                }
+            }
+
+            Console.Write("정수 하나를 입력해주세요 : ");
+            text = Console.ReadLine();
+            if(int.TryParse(text, out result))
+            {
+                if(result < 100 && result > 0)
+                {
+                    Console.WriteLine($"그래, {result}는 100과 0사이의 값이야");
+                }
+                else
+                {
+                    Console.WriteLine($"아니야, {result}는 100과 0사이의 없는 값이야");
+                }
+            }
+        }
+    }
+}
---
+++ ifExample/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ifExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //화면에 "기계 구조용 탄소강의 부피 및 무게 계산기" 출력
+            //1. 재료의 치수(반지름, 높이)를 입력받기
+            //탄소강의 밀도 = 7.85,
+            //2. 부피 계산 : 파이 * 반지름 * 반지름 * 높이
+            //3. 무게 계산 : 부피 * 밀도
+            //Math.PI
+            //double value = 124154.23623098d;
+            //Console.WriteLine($"{value:F3}");
+
+            // 부피가 1000이 넘으면서 무게가 1000kg이 넘는지 출력하기
+            //부피와 무게를 화면에 출력해주세요.
+
+            Console.WriteLine("기계 구조용 탄소강(S45C)의 부피 및 무게 계산기");
+            Console.Write("탄소강의 둘레를 입력해주세요 : ");
+            double radius = 0d;
+            if(double.TryParse(Console.ReadLine(), out double result))
+            {
+                radius = result;
+            }
+
+            Console.Write("탄소강의 높이를 입력해주세요 : ");
+            double height = 0d;
+            if (double.TryParse(Console.ReadLine(), out result))
+            {
+                height = result;
+            }
+
+            double volume = Math.PI * radius * radius * height;
+            double mass = volume * 7.85d;
+
+            if(volume > 1000 && mass > 1000)
+            {
+                Console.WriteLine($"탄소강의 부피와 무게가 모두 1000이 넘습니다.");
+            }
+            else
+            {
+                Console.WriteLine($"탄소강의 부피와 무게가 1000이 안될 수 있습니다.");
+            }
+
+            Console.WriteLine($"탄소강의 부피는 {volume:F3}m^3이며 무게는 {mass:F3}kg입니다");
+        }
+    }
+}
